56fc8acb75d5c53a8ae002ddb96bb9139f8171a8
Trang Huynh <trang.huynh@amplicade.com>
2022-03-28 01:50:29 +0700

    Update client config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,5 +4,5 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   domain: 'quangtrang11111.eu.auth0.com',            // Auth0 domain
   clientId: 'sOjsoccfuH0oYl7RO1AmEtiPHeINBBKt',          // Auth0 client id
-  callbackUrl: 'http://my-album-app.s3-website-us-west-1.amazonaws.com/callback'
+  callbackUrl: 'https://d3brsenoyybkf3.cloudfront.net/callback'
 }
